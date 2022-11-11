4433ee0bd9afa3a674d2cffb85e7b11684c2bd14
mattqze <96447310+mattqze@users.noreply.github.com>
2022-11-11 16:16:32 -0500

    Add files via upload

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,10 +12,11 @@ namespace ConsoleApp16
     {
         static void Main(string[] args)
         {
-            //Define variables for 0 argument operations.
+            //Define variables for argument 0 operations.
             double answer = 0;
             string first1 = null;
             string second1 = null;
+            //Use try/catch due to index out of range error.
             try
             {
                 first1 = args[0];
@@ -28,8 +30,11 @@ namespace ConsoleApp16
 
             if (args[0] == "sqrt")
             {
-                answer = Math.Sqrt(int.Parse(second1));
+                //Calculate Square Root of first argument.
+                answer = Math.Sqrt(int.Parse(args[1]));
+                //Display answer.
                 Console.Write(answer);
+                //End.
                 return;
             }
             //Define Numbers for normal operations.
@@ -37,8 +42,8 @@ namespace ConsoleApp16
             double second = 0;
             try
             {
-                first = int.Parse(args[0]);
-                second = int.Parse(args[2]);
+                first = Convert.ToDouble(args[0]);
+                second = Convert.ToDouble(args[2]);
             }
             catch(System.FormatException)
             {
@@ -48,35 +53,84 @@ namespace ConsoleApp16
             {
                 //Check arguments for math operation sign, do work based on what sign.
                 case "+":
+                    //Add first number to second number.
                     answer = first + second;
+                    //Display answer.
                     Console.Write(answer);
+                    //End.
                     break;
                 case "-":
+                    //Subtract first number from second number.
                     answer = first - second;
+                    //Display answer.
                     Console.Write(answer);
+                    //End.
                     break;
                 case "/":
+                    //Divide first number by second number.
                     answer = first / second;
+                    //Display answer.
                     Console.Write(answer);
+                    //End.
                     break;
                 case "x":
+                    //Multiply first number by second number.
                     answer = first * second;
+                    //Display answer.
                     Console.Write(answer);
+                    //End.
                     break;
                 case "%o":
+                    //Divid first number by 100 then multiply by second number. 
                     answer = first / 100 * second;
+                    //Display answer.
                     Console.Write(answer);
+                    //End.
                     break;
                 default:
+                    //Get all arguments.
                     string log = string.Join(" ", args);
                     //If first argument = math operation, write out error, try to correct user.
                     if (args[0] == "+" || args[0] == "-" || args[0] == "x" || args[0] == "/")
                     {
                         Console.WriteLine("Error: Math Operation in Wrong Place.");
-                        Console.Write("Did you mean: "+ args[1]+" " +args[0]+" " + args[2] +"?");
-                        return;
+                        Console.Write("Did you mean: "+ args[1]+" " +args[0]+" " + args[2] +"? \n[Y/N]\n");
+                        ConsoleKeyInfo cki = Console.ReadKey(true);
+                        if(cki.Key == ConsoleKey.Y)
+                        {
+                            if (args[0] == "+")
+                            {
+                                answer = Convert.ToDouble(args[1]) + Convert.ToDouble(args[2]);
+                            }
+                            if (args[0] == "-")
+                            {
+                                answer = Convert.ToDouble(args[1]) - Convert.ToDouble(args[2]);
+                            }
+                            if (args[0] == "x")
+                            {
+                                answer = Convert.ToDouble(args[1]) * Convert.ToDouble(args[2]);
+                            }
+                            if (args[0] == "/")
+                            {
+                                answer = Convert.ToDouble(args[1]) / Convert.ToDouble(args[2]);
+                            }
+                            Console.Write(answer);
+                            return;
+                        }
+                        if(cki.Key == ConsoleKey.N)
+                        {
+                            Console.Write("Canceled");
+                            return;
+                        }
+                        else
+                        {
+                            Console.Write("Canceled");
+                            return;
+                        }
                     }
+                    //Display error + argument log.
                     Console.Write("Error: " + log);
+                    //End.
                     break;
             }
         }
